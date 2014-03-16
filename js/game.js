6a1961f8798596c593831ed149ba714380206095
Glen Henry <spritewrench@hotmail.com>
2014-03-16 15:32:56 -0500

    initial commit

---
+++ js/game.js
@@ -0,0 +1,234 @@
+(function() {
+  'use strict';
+
+  function Game() {
+    this.player = null;
+    this.image = null;
+    this.debris = null;
+    
+    
+    
+    
+    this.dodgeSpeed = 0.2;
+    this.speed = null;
+    this.target = null;
+    this.playerMoving = false;
+    this.speedHolder =null;
+    this.dodgeHolder = null;
+    this.score = 0;
+    this.scoreText = null;
+    this.bgLine = null;
+    this.fgLine = null;
+    this.linespeed = null;
+    this.hp = 5;
+    this.index = 0;
+
+  }
+
+  Game.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+      this.target = x;
+
+      
+      this.bgLine = [0];
+      for(var i = 0; i < 100;i++){
+        var random = Math.floor((Math.random()*480)+0);
+        this.bgLine[i] = this.add.sprite(random, 0, 'bgLine');
+        this.bgLine[i].anchor.setTo(0.5, 0.5);      
+      }
+      
+      this.image = this.add.sprite(x, 150, 'image');
+      this.image.width = 32;
+      this.image.height = 32;
+      this.image.anchor.setTo(0.5, 0.5);    
+      this.player = this.add.sprite(x, 150, 'player');
+      this.player.width = 16;
+      this.player.height = 16;
+      this.player.anchor.setTo(0.5, 0.5);
+      this.input.onDown.add(this.onInputDown, this);      
+      
+      
+      var val = 80;
+      var place= 800;
+      this.debris = [3];
+      this.speed= [3];
+      for(var i = 0; i < 3; i++ ){
+        var random = Math.floor((Math.random()*2)+1);
+        var extra =0;
+        if(random == 1){
+          extra = -300;
+        }
+        else{
+          extra = 300
+        }    
+        place+=extra;
+        this.debris[i] = this.add.sprite(val, place, 'player');
+        val+=80;
+        place = 800;
+        random = Math.floor((Math.random()*2)+1);
+        if(random == 1){
+          extra = -300;
+        }
+        else{
+          extra = 300
+        }    
+        place+=extra;        
+        this.debris[i].width = 64;
+        this.debris[i].height = 64;
+        this.debris[i].anchor.setTo(0.5, 0.5);        
+        this.speed[i] = 200;
+        
+       
+      }
+
+      this.fgLine = [0];
+      this.linespeed = [0];
+      for(var i = 0; i < 100;i++){
+        var random = Math.floor((Math.random()*320)+1);
+
+        this.fgLine[i] = this.add.sprite(random, 0, 'fgLine');
+        this.fgLine[i].anchor.setTo(0.5, 0.5);  
+        this.linespeed[i] = 100;
+      }       
+      
+      this.scoreText = this.add.bitmapText(x, 400, ''+this.score+'ft', {font: '12px minecraftia', align: 'center'});
+      this.scoreText.anchor.setTo(0.5, 0.5);      
+           
+      
+      this.index = 0;
+      this.speedHolder= this.speed[0];
+      this.dodgeHolder = this.dodgeSpeed;     
+      
+
+
+    },
+
+    update: function () {
+      this.scoreText.setText(this.score+'ft');
+      this.score += 1;
+      this.player.x += (this.target - this.player.x)*this.dodgeSpeed;
+      
+      //image follow
+      this.image.x += (this.player.x - this.image.x)*0.1;
+      
+      //beat twitching
+      this.image.width += (60 - this.image.width)*0.1;
+      this.player.width += (32 - this.player.width)*0.1;
+      this.player.height = this.player.width;
+      this.image.height = this.image.width;
+      //this.player.height += (32 - this.player.width)*0.1;
+      
+      if(32-this.player.width <= 1){
+        this.player.width = 16;
+        this.image.width = 16;
+        //alert('test');
+      }
+
+
+      
+      
+      if(this.player.x != this.target){
+        this.player.angle += 10;
+        this.image.angle += 10;
+      }
+      //tweak player height
+      //Math.round(this.player.y);
+      //debris behaviour 
+      for(var i=0; i < this.debris.length; i++){
+        this.debris[i].body.velocity.y = -(this.speed[i]); //fly up
+        if(this.debris[i].y < 0){
+          var extra = 0;
+          var random = Math.floor((Math.random()*2)+1);
+          if(random == 1){
+            extra = -300;
+          }
+          else{
+            extra = 300
+          }
+          this.debris[i].y = 800+extra;
+          this.speed[i] += 50;
+          if(this.speed[i] > 400){
+            this.speed[i] = 400;
+          }
+
+        }
+
+        
+
+
+        //collision
+        this.physics.collide(this.player, this.debris[i], this.collisionHandler, null, this);        
+      }
+
+      //action lines
+      for(var i=0; i < 100; i++){
+        this.fgLine[i].body.velocity.y = -(this.linespeed[i]); //fly up
+        this.bgLine[i].body.velocity.y = -(this.linespeed[i]); //fly up
+        if(this.bgLine[i].y < 0){
+
+          var randomy = Math.floor((Math.random()*800)+500);
+          var randomx = Math.floor((Math.random()*480)+0);
+          this.bgLine[i].y = randomy;
+          this.bgLine[i].x = randomx;
+
+        } 
+        if(this.fgLine[i].y < 0){
+
+          
+          var randomy = Math.floor((Math.random()*800)+500);
+          var randomx = Math.floor((Math.random()*480)+0);
+          this.fgLine[i].y = randomy;
+          this.fgLine[i].x = randomx;
+          
+          this.linespeed[i]+=50;
+          if(this.linespeed[i] > 1000){
+            this.linespeed[i] = 1000;
+          }
+
+        }         
+      }
+      
+    },
+    collisionHandler: function (obj1, obj2) {
+
+        //  The two sprites are colliding
+        //alert("dead");
+      //alert( (this.player.x-this.target === 0)+' && '+(this.debris.y - this.player.y <= 150));
+      
+      //this.speed = 150;
+        this.speed[this.index] = this.speedHolder;
+        this.dodgeSpeed = this.dodgeHolder;      
+        this.score = 0;
+        this.multiplier = 1;
+        this.game.state.start('menu');      
+
+    },
+    onInputDown: function () {
+      //dodge
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+      var cursorx;
+      x = this.input.position.x;
+      if(x > this.player.x && this.target < 240){
+        this.target += 80;
+      }
+      if(x < this.player.x && this.target > 80){
+        this.target -= 80;
+      }
+      if(x === this.player.x){
+      }
+
+      
+
+      //this.game.state.start('menu');
+    }
+
+  };
+
+  window[''] = window[''] || {};
+  window[''].Game = Game;
+
+}());
---
+++ js/preloader.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  function Preloader() {
+    this.asset = null;
+    this.ready = false;
+    
+  }
+
+  Preloader.prototype = {
+
+    preload: function () {
+      this.asset = this.add.sprite(160, 240, 'preloader');
+      this.asset.anchor.setTo(0.5, 0.5);
+
+      //this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+      this.load.setPreloadSprite(this.asset);
+      this.load.image('player', 'assets/player.png');
+      this.load.image('image', 'assets/afterImage.png');
+      this.load.image('fgLine', 'assets/fgLine.png');
+      this.load.image('bgLine', 'assets/bgLine.png');
+      this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
+      //music from @LimeFaceX
+      this.load.audio('Hail&Release', ['assets/Hail&Release.ogg']);         
+    },
+
+    create: function () {
+      this.asset.cropEnabled = false;
+    },
+
+    update: function () {
+      if (this.ready===false && this.cache.isSoundDecoded('Hail&Release') ) {
+        this.game.state.start('menu');
+      }
+    },
+
+    onLoadComplete: function () {
+      this.ready = true;
+    }
+  };
+
+  window[''] = window[''] || {};
+  window[''].Preloader = Preloader;
+
+}());
